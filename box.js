663d50c8f32327a6721aa09fb717d8e5125905a7
HydreoX <71584025+HydreoX@users.noreply.github.com>
2021-01-16 17:59:17 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,19 @@
+class Box{
+
+constructor(x,y,width,height){
+this.body = Bodies.rectangle(x,y,width,height) 
+World.add(world,this.body)
+
+this.width=width
+this.height=height
+
+
+}
+
+display(){
+rect(this.body.position.x,this.body.position.y,this.width,this.height) 
+
+
+}
+
+}
